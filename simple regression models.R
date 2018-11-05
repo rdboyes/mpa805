996b5f3fc4f4996d3d217fe81035c2ba8c47121d
randy <randy.boyes@gmail.com>
2018-11-05 14:50:12 -0500

    initial commit

---
+++ simple regression models.R
@@ -0,0 +1,21 @@
+install.packages('sjPlot')
+library(sjPlot)
+
+#The whole model
+
+model1 <- lm(`Sale Price` ~ Bds + Ba + `Condo?` + Garage + Queens + `Sq ft`, data = kingston_housing)
+
+#Only houses
+
+model2 <- lm(`Sale Price` ~ Bds + Ba + `Condo?` + Garage + Queens + `Sq ft`, data = kingston_housing[kingston_housing$`Condo?` == 0,])
+
+#Only condos
+
+model3 <- lm(`Sale Price` ~ Bds + Ba + `Condo?` + Garage + Queens + `Sq ft`, data = kingston_housing[kingston_housing$`Condo?` == 1,])
+
+#Cut model down to best predictors
+
+model4 <- lm(`Sale Price` ~ Ba + `Sq ft`, data = kingston_housing[kingston_housing$`Condo?` == 0,])
+
+tab_model(model2)
+
